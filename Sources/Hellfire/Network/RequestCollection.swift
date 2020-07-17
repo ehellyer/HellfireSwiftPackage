f40a3b17e7a3a77c8e03d8c624c106bf36dbcf0e
Ed Hellyer <ehellyer@comcast.net>
2020-07-17 13:46:34 -0400

    Added missing calls to cancel UrlSessionTask.

--- Sources/Hellfire/Network/RequestCollection.swift
+++ Sources/Hellfire/Network/RequestCollection.swift
@@ -9,19 +9,19 @@
 import Foundation
 
 internal class RequestCollection {
-
+    
     //Class setup
     
     init() {
         let queueLabel = "ThreadSafeMessageQueue." + String.randomString(length: 12)
         self.serialMessageQueue = DispatchQueue(label: queueLabel)
     }
-
+    
     //Private API
-
+    
     //Queue used to ensure synchronous access to the 'requests' collection and the index 'taskIndex'
     private var serialMessageQueue: DispatchQueue
-
+    
     //Type definition of Task and Request.
     private typealias TaskRequestPair = (task: URLSessionTask, request: URLRequest)
     
@@ -31,33 +31,36 @@ internal class RequestCollection {
     //Index of the tasks
     private var taskIndex = [RequestTaskIdentifier: TaskRequestPair]()
     
-    ///Public API
+    ///Internal API
     
-    func removeTask(forRequest request: URLRequest) {
+    internal func removeTask(forRequest request: URLRequest) {
         self.serialMessageQueue.sync {
             if let key = self.requests.removeValue(forKey: request) {
                 let _ = self.taskIndex.removeValue(forKey: key.taskIdentifier)
-            }
-        }
-    }
-
-    func removeRequest(forTaskIdentifier taskIdentifier: RequestTaskIdentifier) {
-        self.serialMessageQueue.sync {
-            if let key = self.taskIndex.removeValue(forKey: taskIdentifier) {
-                let _ = self.requests.removeValue(forKey: key.request)
+                key.cancel()
             }
         }
     }
     
-    func add(request: URLRequest, task: URLSessionTask) {
+    internal func removeRequest(forTaskIdentifier taskIdentifier: RequestTaskIdentifier) {
+        self.serialMessageQueue.sync {
+            if let key = self.taskIndex.removeValue(forKey: taskIdentifier) {
+                let request = self.requests.removeValue(forKey: key.request)
+                request?.cancel()
+            }
+        }
+    }
+    
+    internal func add(request: URLRequest, task: URLSessionTask) {
         self.serialMessageQueue.sync {
             let _ = self.requests.updateValue(task, forKey: request)
             let _ = self.taskIndex.updateValue((task, request), forKey: task.taskIdentifier)
         }
     }
     
-    func allTasks() -> [RequestTaskIdentifier] {
+    internal func allTasks() -> [RequestTaskIdentifier] {
         let taskIdentifiers = self.taskIndex.compactMap { $0.key }
         return taskIdentifiers
     }
 }
+
